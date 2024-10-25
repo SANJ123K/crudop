1bdf619226f9938cdb596735a6ed8f4e5aacbdf0
sanj123-dev <sanjeev.kumar@mamsys.com>
2024-10-25 17:24:56 +0530

    op

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = require("./routes/router.js");
 const authRouter = require("./routes/authRouter.js")
 const enrollRouter = require("./routes/studentEnroll.js")
+const cors = require('cors')
 const app = express();
-
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json())
-
+app.use(cors())
 app.use('/api/v1/student/article', router)
 app.use('/api/v1/student', authRouter)
 app.use('/api/v1/student',enrollRouter)
