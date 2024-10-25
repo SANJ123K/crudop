d8d58aab873f28fcf535a449fb4ae70a46a49541
Sanjeev kumar <114824397+SANJ123K@users.noreply.github.com>
2024-10-25 17:53:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const authRouter = require("./routes/authRouter.js")
 const enrollRouter = require("./routes/studentEnroll.js")
 const cors = require('cors')
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+
 
 app.use(express.json())
 app.use(cors())
